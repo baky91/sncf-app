57eef3244510cb263cfd40151d83570096ac3201
baky91 <rakabdu91@gmail.com>
2025-07-20 19:24:46 +0200

    correction pour deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/sncf-app'>
       <App />
     </BrowserRouter>
   </StrictMode>,
