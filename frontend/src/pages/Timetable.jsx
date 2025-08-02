d5e1344c92bd4a1f459c356c98d5f833dc9351e9
baky91 <rakabdu91@gmail.com>
2025-08-02 22:03:52 +0200

    correction url

--- frontend/src/pages/Timetable.jsx
+++ frontend/src/pages/Timetable.jsx
@@ -23,7 +23,7 @@ function Timetable({ onStationSelected }) {
   const arrRef = useRef()
   
   useEffect(() => {
-    fetch(`http://localhost:3000/api/stations?code=${stationCode}`)
+    fetch(`${import.meta.env.VITE_API_URL}/api/stations?code=${stationCode}`)
     .then(res => res.json())
     .then(data => {
       setStation(data.stations[0])
